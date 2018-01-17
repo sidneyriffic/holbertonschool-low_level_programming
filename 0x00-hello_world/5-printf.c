c198457a24fdab28f5164274bb499d2d25918e2e
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-01-17 13:35:11 -0800

    Fixed comment format

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - A simple program to output a line of text
+ *
+ * Return: 0 on completion
+ */
 int main(void)
 {
 	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
