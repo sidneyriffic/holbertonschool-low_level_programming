a460d359c8b98af6e18c638c90291dc14f79db41
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-01-17 12:45:31 -0800

    Initial submission

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("%s\n","with proper grammar, but the outcome is a piece of art,");
+  return 0;
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+  printf("%s%i%s\n","Size of a char: ",sizeof(char)," byte(s)");
+  printf("%s%i%s\n","Size of an int: ",sizeof(int)," byte(s)");
+  printf("%s%i%s\n","Size of a long int: ",sizeof(long int)," byte(s)");
+  printf("%s%i%s\n","Size of a long long int: ",sizeof(long long int)," byte(s)");
+  printf("%s%i%s\n","Size of a float: ",sizeof(float)," byte(s)");
+}
