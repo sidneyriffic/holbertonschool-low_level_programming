85ff57781e333897c3159aac8b0a358edb6e5f1a
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-01-17 13:31:29 -0800

    Bettydoc compliance

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 
+
+/**
+ * main - A simple program that outputs a line of text using puts() 
+ *
+ * Return: 0 upon completion
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
