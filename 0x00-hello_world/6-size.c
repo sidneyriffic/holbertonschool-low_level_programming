bf6a4931b882764153b4c446cb90a4edbafd544c
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-01-17 13:00:36 -0800

    Added return

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,4 +7,5 @@ int main()
   printf("%s%i%s\n","Size of a long int: ",sizeof(long int)," byte(s)");
   printf("%s%i%s\n","Size of a long long int: ",sizeof(long long int)," byte(s)");
   printf("%s%i%s\n","Size of a float: ",sizeof(float)," byte(s)");
+  return 0;
 }
