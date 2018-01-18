707126ec8ad2c6e5ff8920adaf1a8a9e36b64f32
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-01-18 12:55:40 -0800

    Fixed bug introduced by formatting change

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	printf("Last digit of ");
-	if (n % 10 > 6 || n % 10 < 6)
+	if (n % 10 > 6 || n % 10 < -6)
 		printf("%d is %d and is greater than 5\n", n, n % 10);
 	else if (n % 10 > 0 || n % 10 < 0)
 		printf("%d is %d and is less than 6 and not 0\n", n, n % 10);
