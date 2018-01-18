92bbd779778c404c8ba829b5b03a87fc1e1f1172
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-01-17 16:43:26 -0800

    Testing revised fwrite version against checker

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
  * main - a simple program to write a line of text, this time without the
@@ -9,6 +8,8 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char b[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(b, sizeof(char), sizeof(b) - 1, stderr);
 	return (1);
 }
