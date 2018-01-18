89e24bdfe6334581b23f4bf5257a9e10f16ae84b
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-01-18 13:02:58 -0800

    typo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	printf("Last digit of ");
 	if (n % 10 > 5)
 		printf("%d is %d and is greater than 5\n", n, n % 10);
-	else if (n % 10 > 0)
+	else if (n % 10 != 0)
 		printf("%d is %d and is less than 6 and not 0\n", n, n % 10);
 	else
 		printf("%d is %d and is 0\n", n, n % 10);
