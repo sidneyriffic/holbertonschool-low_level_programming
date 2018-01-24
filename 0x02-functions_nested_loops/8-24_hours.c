bb40d4ee86ab702e34aea7313257528e4c4c290f
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-01-23 17:36:22 -0800

    Translated included helper to function in source

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,27 @@
 #include "holberton.h"
 
 /**
+ * _printstr - prints a given string
+ * @c: a null temrinated character array
+ *
+ * Return: 0 if successful
+ */
+int _printstr(char c[])
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+		_putchar(c[i++]);
+
+	return (0);
+}
+
+/**
  * jack_bauer - prints times from 00:00 to 24:00
  *
  * Return: 0 on completion
  */
-int jack_bauer(void)
+void jack_bauer(void)
 {
 	char x[6] = "00:00\n";
 
