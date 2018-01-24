17cb17ae3e51ec8f59ee2024fd72d05274986d83
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-01-23 16:50:15 -0800

    Fixed function name

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,15 @@
+#include "holberton.h"
+
 /**
- * isalpha - returns whether c is an alpha character
+ * _isalpha - returns whether c is an alpha character
  *
  * @c: ascii value of a character we're determining if is alpha
  *
  * Return: 1 is c is a letter, 0 otherwise
  */
-int isalpha(int c)
+int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' || c <= 'z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	return (0);
 }
