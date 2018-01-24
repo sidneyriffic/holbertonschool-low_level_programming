c869060866ae1f1135b821c5030615fbb3c1c682
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-01-23 16:10:45 -0800

    Converted to all-in-one

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,6 +1,20 @@
 #include "holberton.h"
 
 /**
+ * _printstr - print a given string using _putchar
+ * @x: a null terminated character array to be printed
+ * Return: 0 on success
+ */
+int _printstr(char x[])
+{
+	int i = 0;
+
+	while (x[i] != '\0')
+		_putchar(x[i++]);
+	return (0);
+}
+
+/**
  * main - a simple program that outputs "Holberton"
  * Return: 0 on success
  */
