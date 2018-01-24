70b603ca815033b706bee070c99c0b90f8f4746e
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-01-23 16:17:35 -0800

    Reducing to one function for checker checking

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,25 +1,15 @@
 #include "holberton.h"
 
 /**
- * _printstr - print a given string using _putchar
- * @x: a null terminated character array to be printed
- * Return: 0 on success
- */
-int _printstr(char x[])
-{
-	int i = 0;
-
-	while (x[i] != '\0')
-		_putchar(x[i++]);
-	return (0);
-}
-
-/**
  * main - a simple program that outputs "Holberton"
  * Return: 0 on success
  */
 int main(void)
 {
-	_printstr("Holberton\n");
+	char str[] = "Holberton\n";
+	int i = 0;
+
+	while (str[i] != '\0')
+		_putchar(str[i]);
 	return (0);
 }
