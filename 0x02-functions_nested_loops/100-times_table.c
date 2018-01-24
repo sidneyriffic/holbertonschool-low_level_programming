c2515384170d3f36f1b8bf338767aeef2b20379d
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-01-23 17:52:54 -0800

    translated helper function from include to source

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,26 @@
 #include "holberton.h"
+
+/**
+ * _printstr - prints a given string
+ * @c: a null temrinated character array
+ *
+ * Return: 0 if successful
+ */
+int _printstr(char c[])
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+		_putchar(c[i++]);
+
+	return (0);
+}
+
 /**
  * print_times_table - prints out an arbitrary multiplication table between
  * 1 and 15
+ *
+ * @n: integer that defines dimnsions for the table
  *
  * Return void
  */
@@ -10,7 +29,7 @@ void print_times_table(int n)
 	char buffer[5] = ",    ";
 	int i, j, num;
 
-	if (n < 1 or n > 15)
+	if (n < 1 || n > 15)
 		return;
 	for (i = 0; i <= n; i++)
 	{
