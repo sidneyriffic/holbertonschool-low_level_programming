287907b6c41cf5027449957e7cc17c4f41c2a1cb
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-01-25 15:28:43 -0800

    fixed accidental editsand off by one

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,11 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 15; j++)
+		for (j = 0; j < 15; j++)
 		{
 			if (j / 10 > 0)
 				_putchar(j / 10 + '0');
-			_putchar(j + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
