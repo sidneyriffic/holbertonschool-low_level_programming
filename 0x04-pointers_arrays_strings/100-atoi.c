52247389d137fabaddd82a323c136c3bd82e151d
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-01-30 02:24:22 -0800

    trying alternate +/- computation method

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -7,16 +7,14 @@
  */
 int _atoi(char *s)
 {
-	int signs = 0;
+	int signs = 1;
 	unsigned int sum = 0;
 	int final = 0;
 
 	for (; *s != '\0'; s++)
 	{
-		if (*s == '+')
-			signs++;
-		else if (*s == '-')
-			signs--;
+		if (*s == '-')
+			signs *= -1;
 		else if (*s >= '0' && *s <= '9')
 			break;
 	}
@@ -27,7 +25,7 @@ int _atoi(char *s)
 		s++;
 	}
 
-	if (signs < 0)
+	if (signs == -1)
 		final = -sum;
 	else
 		final = sum;
