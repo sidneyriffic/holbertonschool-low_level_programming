cdb8276ac451f7f1267c2aed80a775aa605ed5c2
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-01 19:44:17 -0800

    Hidden requirement

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -13,9 +13,9 @@ int _strcmp(char *s1, char *s2)
 	while (*s1 != 0 || *s2 != 0)
 	{
 		if (*s1 > *s2)
-			return (1);
+			return (*s1 - *s2);
 		if (*s1 < *s2)
-			return (-1);
+			return (*s1 - *s2);
 		s1++;
 		s2++;
 	}
