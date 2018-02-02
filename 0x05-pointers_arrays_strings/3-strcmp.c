03482137e9209161bb75eb495e0cba98b4bb8f98
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-01 18:52:30 -0800

    Handle empty string

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != 0)
+	while (*s1 != 0 || *s2 != 0)
 	{
 		if (*s1 > *s2)
-			return (15);
+			return (1);
 		if (*s1 < *s2)
-			return (-15);
+			return (-1);
 		s1++;
 		s2++;
 	}
