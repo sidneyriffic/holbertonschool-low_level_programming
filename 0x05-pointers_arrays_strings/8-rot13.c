ee3d3bcead70d404aa23decd8f9d98a13ff78ea9
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-01 21:08:44 -0800

    Initial submission

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,27 @@
+/**
+ * rot13 - encodes a string in rot13
+ *
+ * @a: string to encode
+ *
+ * Return: encoded string
+ */
+char *rot13(char *a)
+{
+	char *sptr = a;
+	int index;
+	char translation[2][54] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+				   "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+
+	while (*sptr != 0)
+	{
+		if ((*sptr >= 'a' && *sptr <= 'z') || (*sptr >= 'A' && *sptr <= 'Z'))
+		{
+			index = 0;
+			while (*sptr != translation[0][index])
+				index++;
+			*sptr = translation[1][index];
+		}
+		sptr++;
+	}
+	return a;
+}
