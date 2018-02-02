63b353ab1a7eeebe1e6a0d787594212bd5b3dbc2
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-01 19:59:21 -0800

    Betty fix

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -16,7 +16,7 @@ char *cap_string(char *a)
 		if (newword == 1)
 		{
 			newword = 0;
-			if(*ptr < 'z' && *ptr > 'a')
+			if (*ptr < 'z' && *ptr > 'a')
 				*ptr -= 32;
 		}
 		if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' || *ptr == ','
