f739138392607af4938d1465a767f2503dd25f55
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-01 19:58:05 -0800

    Initial submission

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+/**
+ * cap_string - capitalize all words in a string.
+ * separators: space tab newline ,;.!?"(){}
+ *
+ * @a: character string to capitalize
+ *
+ * Return: string
+ */
+char *cap_string(char *a)
+{
+	int newword = 1;
+	char *ptr = a;
+
+	while (*ptr != 0)
+	{
+		if (newword == 1)
+		{
+			newword = 0;
+			if(*ptr < 'z' && *ptr > 'a')
+				*ptr -= 32;
+		}
+		if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' || *ptr == ','
+		    || *ptr == ';' || *ptr == '.' || *ptr == '!' || *ptr == '"'
+		    || *ptr == '(' || *ptr == ')' || *ptr == '{' || *ptr == '}')
+			newword = 1;
+		ptr++;
+	}
+	return (a);
+}
