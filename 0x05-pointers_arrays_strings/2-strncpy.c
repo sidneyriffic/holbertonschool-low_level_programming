7367db267494d74225d016e77401a802ab5cc1b5
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-01 20:25:35 -0800

    converted from pointer to array notation

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -11,14 +11,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *ptr = dest;
+	int i = 0;
 
 	while (n > 0)
 	{
-		*ptr = *src;
-		ptr++;
-		src++;
-		if (*src == '\0')
+		dest[i] = src[i];
+		i++;
+		if (src[i] == '\0')
 			break;
 		n--;
 	}
@@ -26,8 +25,8 @@ char *_strncpy(char *dest, char *src, int n)
 	if (n > 0)
 		while (n)
 		{
-			*ptr = 0;
-			ptr++;
+			dest[i] = 0;
+			i++;
 			n--;
 		}
 
