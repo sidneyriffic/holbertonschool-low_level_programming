4a0fd020d9ebd613959deca2547c3db7dc464570
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-05 17:14:38 -0800

    Initial submission

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - prints a given 2d array chess board of chars
+ *
+ * char *(a)[8]);
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
+}
