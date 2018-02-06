90bda0f93c3da514aab7336ba8031f164584c99c
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-05 17:31:59 -0800

    Initial submission of 8

---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * print_diagsums - prints both sums of diagonal numbers in a 2d array
+ *
+ * @a: integer matrix
+ * @size: square size of matrix
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int n = 0, sum = 0;
+	int *ptr = a;
+
+	for (n = size; n > 0; n--)
+	{
+ 		sum += *ptr;
+		ptr += size + 1;
+	}
+	printf("%d, ",sum);
+
+	sum = 0;
+	ptr = a + size - 1;
+	for (n = size; n > 0; n--)
+	{
+		sum += *ptr;
+		ptr += size - 1;
+	}
+	printf("%d\n",sum);
+}
