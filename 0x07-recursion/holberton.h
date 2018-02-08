a61be57b6ccd3b761facd0c576c233c049800401
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-07 16:23:34 -0800

    Add header contents

--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -1,6 +1,13 @@
 #ifndef HOLBERTONH
 #define HOLBERTONH
 
-
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
 
 #endif
