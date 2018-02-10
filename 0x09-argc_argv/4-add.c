64f564290a79d8a9439b482f901c1ad18f5c75e8
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-10 13:57:05 -0800

    Fix 0 and 9 on first character of arg

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!(argv[i][0] > '0' && argv[i][0] < '9')
+		if (!(argv[i][0] >= '0' && argv[i][0] <= '9')
 		    && argv[i][0] != '-')
 		{
 			printf("Error\n");
