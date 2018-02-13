a7536bcc94d3c032484d882865b7eb296f50bfa5
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-13 14:13:48 -0800

    Add 1-strdup.c

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string into newly allocated array
+ *
+ * @str: string to duplicate
+ *
+ * Return: pointer to new string
+ */
+char *_strdup(char *str)
+{
+	int size = 0;
+	char *ptr, *ret;
+
+	if (!str)
+		return (NULL);
+
+	ptr = str;
+	while (*ptr++)
+		size++;
+
+	ret = malloc(size + 1);
+	if (!ret)
+		return (NULL);
+
+	ptr = ret;
+	while (*str)
+		*ptr++ = *str++;
+
+	*ptr = 0;
+	return (ret);
+}
