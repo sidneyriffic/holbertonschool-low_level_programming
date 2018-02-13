15e257dc4e6094c540a7f2bffb2a321f6e6caa0d
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-12 20:00:45 -0800

    Initial Submission

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of size size
+ *
+ * @size: size of array
+ * @c: char to fill array with
+ *
+ * Return: pointer to first element
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *first;
+	unsigned int i;
+
+	if (size <= 0)
+		return (NULL);
+
+	first = malloc(size);
+	for (i = 0; i < size; i++)
+		first[i] = c;
+
+	return (first);
+}
