1e66066c5026915142a84fd1cb4f93ec74d35b09
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-13 15:15:38 -0800

    Properly handle width or height of 0

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@ int **alloc_grid(int width, int height)
 	int **grid, *ptr;
 	int i, j;
 
-	if (width < 0 || height < 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	grid = malloc(height * (int) sizeof(int *));
