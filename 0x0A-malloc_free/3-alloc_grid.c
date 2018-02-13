d91a9999f0c7ceab2133bcf86aac81cf8d57cbfd
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-13 15:13:50 -0800

    Add alloc_grid.c

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+/**
+ * alloc_grid - allocates a matrix of integers
+ *
+ * @width: width of grid
+ * @height: height of grid
+ *
+ * Return: void
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid, *ptr;
+	int i, j;
+
+	if (width < 0 || height < 0)
+		return (NULL);
+
+	grid = malloc(height * (int) sizeof(int *));
+	if (!grid)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		ptr = malloc(width * (int) sizeof(int));
+		if (!ptr)
+			return (NULL);
+		for (j = 0; j < width; j++)
+			ptr[j] = 0;
+		grid[i] = ptr;
+	}
+
+	return (grid);
+}
