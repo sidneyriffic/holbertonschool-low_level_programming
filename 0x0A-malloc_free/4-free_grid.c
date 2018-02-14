ce263abe8c16f52df4ceaf9443a60fc0572df648
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-13 17:06:28 -0800

    Add 4-free_grid.c

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - free up grid allocated by alloc_grid
+ *
+ * @grid - grid to free
+ * @height - height of grid
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	if (!grid)
+		return;
+	if (!grid[0])
+		return;
+	while(height-- > 0)
+		free(grid[height]);
+	free(grid);
+}
