514aba0a69517991da3233487881095a67c3667c
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-13 18:24:59 -0800

    Add 100-strtow.c

---
+++ 0x0A-malloc_free/100-strtow.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+
+/**
+ * strtow - converts a string into its separate words
+ *
+ * @str: string to convert into words
+ *
+ * Return: 2d array pointer
+ */
+char **strtow(char *str)
+{
+	char **ret, *ptr = str;
+	int wc = 0, i = 0;
+
+	if (*str == 0 || str == 0)
+		return (0);
+	while (*ptr)
+	{
+		if (*ptr == ' ' && !(*(ptr + 1) == ' '))
+			wc++;
+		ptr++;
+	}
+	ret = malloc((wc + 1) * sizeof(char *));
+	if (ret == 0)
+		return (0);
+	while (*str)
+	{
+		if (*str != ' ')
+		{
+			for (ptr = str, wc = 0; *ptr != ' ' && *ptr != 0;)
+				wc++, ptr++;
+			ret[i] = malloc(wc + 1);
+			if (ret[i] == 0)
+			{
+				while (i >= 0)
+					free(ret[i--]);
+				free(ret);
+				return (0);
+			}
+			ptr = ret[i];
+			while (*str != ' ' && *str != 0)
+				*ptr++ = *str++;
+			*ptr = 0;
+			str++;
+			i++;
+		}
+		str++;
+	}
+	ret[i] = 0;
+	return (ret);
+}
