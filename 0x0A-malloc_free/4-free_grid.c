9dd250dc2fc24893fefc091babb4ca57ead9ed9c
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-13 17:07:48 -0800

    Appease Betty

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -3,8 +3,8 @@
 /**
  * free_grid - free up grid allocated by alloc_grid
  *
- * @grid - grid to free
- * @height - height of grid
+ * @grid: - grid to free
+ * @height: - height of grid
  *
  * Return: void
  */
@@ -14,7 +14,7 @@ void free_grid(int **grid, int height)
 		return;
 	if (!grid[0])
 		return;
-	while(height-- > 0)
+	while (height-- > 0)
 		free(grid[height]);
 	free(grid);
 }
