2c4369421b31365d83c42fb1ae527f67014c7c6e
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-15 12:38:31 -0800

    Optimize memory use for n

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > len2)
 		n = len2;
 
-	ret = malloc((len1 + len2 + 1) * sizeof(char));
+	ret = malloc((len1 + n + 1) * sizeof(char));
 	if (!ret)
 		return (0);
 
