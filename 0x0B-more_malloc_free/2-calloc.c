25a785af7ab0384619027fb6f8e0b869b119d4cf
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-15 12:42:44 -0800

    Return 0 with alloc size 0

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -13,9 +13,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 	unsigned int i;
 
+	if(nmemb == 0 || size == 0)
+		return (0);
+
 	ret = malloc(nmemb * size);
 	if (ret == 0)
-		return (ret);
+		return (0);
 
 	for (i = nmemb * size, ptr = ret; i; i--)
 		*ptr++ = 0;
