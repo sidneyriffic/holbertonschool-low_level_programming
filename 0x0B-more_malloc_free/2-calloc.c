63df59bac7fb7e666527e189fbe67c4833536c0d
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-15 12:43:45 -0800

    Fix style

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 	unsigned int i;
 
-	if(nmemb == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (0);
 
 	ret = malloc(nmemb * size);
