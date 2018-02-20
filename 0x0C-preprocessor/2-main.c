c4ba8903a496809359b98fe12c3e58362584a022
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-19 16:35:27 -0800

    Fix style

--- 0x0C-preprocessor/2-main.c
+++ 0x0C-preprocessor/2-main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - print name of file main was compiled from
+ *
+ * Return: 0 on success
+ */
+int main(void)
 {
 	printf(__FILE__"\n");
 	return (0);
