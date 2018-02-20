87ed3fc77f1f7e2a4937e3c7db44121cbb982677
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-19 16:55:28 -0800

    Add newlines for error cases

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -13,12 +13,12 @@ void print_dog(struct dog *d)
 	if (d == 0)
 		return;
 	if (d->name == 0)
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	if (d->owner == 0)
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
