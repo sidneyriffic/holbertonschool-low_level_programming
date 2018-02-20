8b4f473b0b053359ff943a9968e0a8421f275058
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-19 17:04:17 -0800

    Add handling for invalid dog address

--- 0x0D-structures_typedef/1-init_dog.c
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -1,8 +1,21 @@
 #include "dog.h"
 
+/**
+ * init_dog - initializes a dog with given values
+ *
+ * @d: dog to initialize
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
