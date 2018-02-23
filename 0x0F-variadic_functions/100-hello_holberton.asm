885c3a9c12666d565cdafb9b9fe96ea652d69d95
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-23 06:37:39 -0800

    Initial submission

---
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,14 @@
+    section .data
+msg:	db	"Hello, Holberton",10
+    global _start
+
+    section .text
+_start:	
+    mov	rax, 1
+    mov	rdi, 1
+    mov	rsi, msg
+    mov	rdx, 17
+    syscall
+    mov rax, 60
+    xor rdi, rdi
+    syscall
