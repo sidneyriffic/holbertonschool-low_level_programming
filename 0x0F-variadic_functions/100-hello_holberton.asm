1e16ab09a1c3245c231681521b7910578968b1ce
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-02-25 03:50:56 -0800

    Use symbol main instead of _start

--- 0x0F-variadic_functions/100-hello_holberton.asm
+++ 0x0F-variadic_functions/100-hello_holberton.asm
@@ -1,11 +1,15 @@
     section .data
 msg:	db	"Hello, Holberton",10
-    global _start
+    global main
 
     section .text
-_start:
+main:
     mov	rax, 1
     mov	rdi, 1
     mov	rsi, msg
     mov	rdx, 17
+    syscall
+
+    mov rax, 60
+    xor rdi, rdi
     syscall
