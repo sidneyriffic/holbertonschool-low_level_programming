77b25ee2e6020c4b1551b2079f0bdfcf00520a18
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-03-01 21:30:43 -0800

    Add 3-free_list.c

---
+++ 0x11-singly_linked_lists/3-free_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - free a list_t list
+ *
+ * @head: head of list to free
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *next;
+
+	if (head == NULL)
+		return;
+	*next = head->next;
+
+	while (head != NULL)
+	{
+		if (head->str != NULL)
+			free(head->str);
+		free(head);
+		head = next;
+		if (head != NULL)
+			next = head->next;
+	}
+}
