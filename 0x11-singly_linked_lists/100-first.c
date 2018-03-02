8449f00821053bc596d3032d86a0ea82cca99f91
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-03-01 21:46:07 -0800

    Add 100-first

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * turtle - prints before main
+ */
+void __attribute__ ((constructor)) turtle()
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
