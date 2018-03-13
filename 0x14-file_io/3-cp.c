1f23909b4921a0b6d386f0bce2ff1c5325403b83
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-03-13 15:34:40 -0700

    Use flags instead of octal mode

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -52,7 +52,8 @@ int main(int ac, char *av[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		return (98);
 	}
-	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC,
+		       S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
