d0565b192ff21afe5af55bcdfa4d32c1cbb7088c
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-03-13 15:14:55 -0700

    Add handling for file close errors

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -22,14 +22,13 @@ int main(int ac, char *av[])
 {
 	char buf[1024];
 	ssize_t lenr, lenw;
-	int file_from, file_to;
+	int file_from, file_to, closefile;
 
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		return (97);
 	}
-
 	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
 	{
@@ -42,7 +41,6 @@ int main(int ac, char *av[])
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		return (99);
 	}
-
 	do {
 		lenr = read(file_from, buf, 1024);
 		if (lenr == -1)
@@ -57,5 +55,9 @@ int main(int ac, char *av[])
 			return (99);
 		}
 	} while (lenr == 1024);
+	if (close(file_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+	if (close(file_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 	return (1);
 }
