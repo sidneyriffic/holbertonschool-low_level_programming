8403959eabf5cf901ce90cda93e9446f79f0b4a2
Sidney Burkett <sidneyaburkett@yahoo.com>
2018-03-14 03:07:06 -0700

    Fix endianness for second 16 bytes

--- 0x14-file_io/100-elf_header.c
+++ 0x14-file_io/100-elf_header.c
@@ -34,7 +34,7 @@ void printdata(char *head)
 	printf("  %-35s", "Data:");
 	if (head[5] == 1)
 		printf("2's complement, little endian\n");
-	else if (head[5] == 1)
+	else if (head[5] == 2)
 		printf("2's complement, big endian\n");
 	else
 		printf("<unknown: %02hx>", head[5]);
@@ -65,43 +65,43 @@ void printversion(char *head)
  */
 void printabi(char *head)
 {
-	printf("  %-35sUNIX - ", "OS/ABI:");
+	printf("  %-35s", "OS/ABI:");
 	if (head[7] == 0)
-		printf("System V\n");
+		printf("UNIX - System V\n");
 	else if (head[7] == 1)
-		printf("HP-UX\n");
+		printf("UNIX - HP-UX\n");
 	else if (head[7] == 2)
-		printf("NetBSD\n");
+		printf("UNIX - NetBSD\n");
 	else if (head[7] == 3)
-		printf("Linux\n");
+		printf("UNIX - Linux\n");
 	else if (head[7] == 4)
-		printf("GNU Hurd\n");
-	else if (head[7] == 5)
-		printf("Solaris\n");
+		printf("UNIX - GNU Hurd\n");
 	else if (head[7] == 6)
-		printf("AIX\n");
+		printf("UNIX - Solaris\n");
 	else if (head[7] == 7)
-		printf("IRIX\n");
+		printf("UNIX - AIX\n");
 	else if (head[7] == 8)
-		printf("FreeBSD\n");
+		printf("UNIX - IRIX\n");
 	else if (head[7] == 9)
-		printf("Tru64\n");
+		printf("UNIX - FreeBSD\n");
 	else if (head[7] == 10)
-		printf("Novell Modesto\n");
+		printf("UNIX - Tru64\n");
 	else if (head[7] == 11)
-		printf("OpenBSD\n");
+		printf("UNIX - Novell Modesto\n");
 	else if (head[7] == 12)
-		printf("Open VMS\n");
+		printf("UNIX - OpenBSD\n");
 	else if (head[7] == 13)
-		printf("NonStop Kernel\n");
+		printf("UNIX - Open VMS\n");
 	else if (head[7] == 14)
-		printf("AROS\n");
+		printf("UNIX - NonStop Kernel\n");
 	else if (head[7] == 15)
-		printf("Fenix OS\n");
+		printf("UNIX - AROS\n");
 	else if (head[7] == 16)
-		printf("CloudABI\n");
+		printf("UNIX - Fenix OS\n");
+	else if (head[7] == 17)
+		printf("UNIX - CloudABI\n");
 	else
-		printf("<unknown %02x", head[7]);
+		printf("<unknown: %02x>\n", head[7]);
 	printf("  %-35s%d\n", "ABI Version:", head[8]);
 }
 
@@ -114,14 +114,20 @@ void printabi(char *head)
  */
 void printtype(char *head)
 {
+	int index;
+
+	if (head[5] == 1)
+		index = 16;
+	else
+		index = 17;
 	printf("  %-35s", "Type:");
-	if (head[16] == 1)
+	if (head[index] == 1)
 		printf("REL (Relocatable file)\n");
-	else if (head[16] == 2)
+	else if (head[index] == 2)
 		printf("EXEC (Executable file)\n");
-	else if (head[16] == 3)
+	else if (head[index] == 3)
 		printf("DYN (Shared object file)\n");
-	else if (head[16] == 4)
+	else if (head[index] == 4)
 		printf("CORE (Core file)\n");
 	else
 		printf("<unknown>: %02x%02x\n", head[16], head[17]);
@@ -136,19 +142,33 @@ void printtype(char *head)
  */
 void printentry(char *head)
 {
-	int i;
+	int i, end;
 
 	printf("  %-35s0x", "Entry point address:");
 	if (head[4] == 2)
-		i = 0x1f;
+		end = 0x1f;
 	else
-		i = 0x1b;
-	while (head[i] == 0 && i > 0x18)
-		i--;
-	printf("%x", head[i--]);
-	while (i >= 0x18)
-		printf("%02x", (unsigned char) head[i--]);
-	printf("\n");
+		end = 0x1b;
+	if (head[5] == 1)
+	{
+		i = end;
+		while (head[i] == 0 && i > 0x18)
+			i--;
+		printf("%x", head[i--]);
+		while (i >= 0x18)
+			printf("%02x", (unsigned char) head[i--]);
+		printf("\n");
+	}
+	else
+	{
+		i = 0x18;
+		while (head[i] == 0)
+			i++;
+		printf("%x", head[i++]);
+		while (i <= end)
+			printf("%02x", (unsigned char) head[i++]);
+		printf("\n");
+	}
 }
 
 /**
