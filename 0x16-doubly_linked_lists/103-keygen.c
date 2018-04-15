f0d6760c912e0c2b0cf4e48d052d686fc5bc2a16
Sidney Burkett <sidneyriffic@gmail.com>
2018-04-14 23:58:15 -0700

    Add 103

---
+++ 0x16-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int ac, char *av[])
+{
+	long nums[] = {0x3877445248432d41, 0x42394530534e6c37,
+			    0x4d6e706762695432, 0x74767a5835737956,
+			    0x2b554c59634a474f, 0x71786636576a6d34,
+			    0x723161513346655a, 0x6b756f494b646850};
+	int len = 0, temp, ct, randh;
+	char *str = (char *) nums, *uname = av[1], *ptr, password[7];
+
+	password[6] = 0;
+	
+/*	while (len < 64)
+	{
+		putchar(str[len++]);
+		}*/
+	for (ptr = uname; *ptr; ptr++)
+		len++;
+	password[0] = str[(len ^ 0x3b) & 0x3f];
+	for (ptr = uname, temp = 0; *ptr; ptr++)
+		temp += *ptr;
+	password[1] = str[(temp ^ 0x4f) & 0x3f];
+	for (ptr = uname, temp = 1; *ptr; ptr++)
+		temp *= *ptr;
+	password[2] = str[(temp ^ 0x55) & 0x3f];
+	for (ptr = uname, temp = *ptr; *ptr; ptr++)
+		if (temp < *ptr)
+			temp = *ptr;
+	srand(temp ^ 0xe);
+	password[3] = str[rand() & 0x3f];
+	for (ptr = uname, temp = 0; *ptr; ptr++)
+		temp += *ptr * *ptr;
+	password[4] = str[(temp ^ 0xef) & 0x3f];
+	for (temp = *uname, ct = 0; ct < temp; ct++)
+		randh = rand();
+	password[5] = str[(randh ^ 0xe5) & 0x3f];
+	printf("%s %s", uname, password);
+	return (0);
+}
