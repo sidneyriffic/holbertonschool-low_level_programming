5de273bd82361b826adebcd7f36bba9dc84a7b23
Sidney Burkett <sidneyriffic@gmail.com>
2018-04-16 16:15:13 -0700

    Add files for mandatory tasks in 0x17

---
+++ 0x17-dynamic_libraries/README.md
@@ -0,0 +1,4 @@
+holberton.h: header file for contents of libholberton.so  
+libholberton.so: library of holberton projects  
+1-create_dynamic_lib.sh: script that will create a dynamic library out of
+			 all .c files in the current directory
---
+++ 0x17-dynamic_libraries/holberton.h
@@ -0,0 +1,25 @@
+#ifndef HOLBERTONH
+#define HOLBERTONH
+
+int _isupper(int);
+char *_memset(char *s, char b, unsigned int n);
+char *_strcat(char *dest, char *src);
+int _atoi(char *s);
+int _isdigit(int c);
+char *_memcpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strchr(char *s, char c);
+int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
+int _islower(int c);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+unsigned int _strspn(char *s, char *accept);
+int _isalpha(int c);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+int _abs(int n);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+
+#endif
