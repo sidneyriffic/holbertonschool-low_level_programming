f7c54c659718b6f9623a84ee9f0adc7a2134025f
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-11 18:48:25 -0700

    Add size 0 as auto fail in 0x19-3

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *newnode;
 
 	if (key == NULL || *key == 0 || ht == NULL
-	    || ht->array == NULL)
+	    || ht->array == NULL || ht->size == 0)
 		return (0);
 	newnode = malloc(sizeof(hash_node_t));
 	if (newnode == NULL)
