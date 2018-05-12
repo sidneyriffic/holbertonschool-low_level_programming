b80c181d9f61c2af06aa5045cea6c5284bbe7b55
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-11 18:31:11 -0700

    Add emtpy string key error case for 0x19-3

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -15,7 +15,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *newnode;
 
-	if (key == NULL || ht == NULL || ht->array == NULL || value == NULL)
+	if (key == NULL || *key == 0 || ht == NULL
+	    || ht->array == NULL || value == NULL)
 		return (0);
 	newnode = malloc(sizeof(hash_node_t));
 	if (newnode == NULL)
