bca8522763b27c363721587c383e6cada793354a
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-11 20:29:27 -0700

    Try not allowing duplicate entries for 0x19-3 checker

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -15,29 +15,48 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *newnode;
+	hash_node_t *newnode, *ptr;
+	char *newval;
 
 	if (key == NULL || *key == 0 || value == NULL || ht == NULL
 	    || ht->array == NULL || ht->size == 0)
 		return (0);
-	newnode = malloc(sizeof(hash_node_t));
-	if (newnode == NULL)
-		return (0);
 	index = key_index((const unsigned char *) key, ht->size);
-	newnode->key = strdup(key);
-	if (newnode->key == NULL)
+	ptr = ht->array[index];
+	while (ptr != NULL)
 	{
-		free(newnode);
-		return (0);
+		if (strcmp(ptr->key, key) == 0)
+			break;
+		ptr = ptr->next;
 	}
-	newnode->value = strdup(value);
-	if (newnode->value == NULL)
+	if (ptr == NULL)
 	{
-		free(newnode->key);
-		free(newnode);
-		return (0);
+		newnode = malloc(sizeof(hash_node_t));
+		if (newnode == NULL)
+			return (0);
+		newnode->key = strdup(key);
+		if (newnode->key == NULL)
+		{
+			free(newnode);
+			return (0);
+		}
+		newnode->value = strdup(value);
+		if (newnode->value == NULL)
+		{
+			free(newnode->key);
+			free(newnode);
+			return (0);
+		}
+		newnode->next = ht->array[index];
+		ht->array[index] = newnode;
 	}
-	newnode->next = ht->array[index];
-	ht->array[index] = newnode;
+	else
+	{
+		newval = strdup(value);
+		if (newval == NULL)
+			return (0);
+		free(ptr->value);
+		ptr->value = newval;
+	}
 	return (1);
 }
