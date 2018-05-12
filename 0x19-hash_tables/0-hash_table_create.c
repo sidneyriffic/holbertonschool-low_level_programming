cec0401170edd9bba111ef5d58ac06db72b9abda
Sidney Burkett <sidneyriffic@gmail.com>
2018-05-11 18:01:37 -0700

    Add some more malloc error cases in 0x19-0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -6,13 +6,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *newtable;
 	unsigned long int idx;
 
+	if (size == 0)
+		return (NULL);
 	newtable = malloc(sizeof(hash_table_t));
 	if (newtable == NULL)
 		return (NULL);
 
 	newtable->array = malloc(sizeof(hash_node_t *) * size);
 	if (newtable->array == NULL)
+	{
+		free(newtable);
 		return (NULL);
+	}
 
 	newtable->size = size;
 	for (idx = 0; idx < size; idx++)
