157134818d555b0dbb448bb3f413ac4a0b0223f4
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-02 16:36:24 -0700

    Add 0x1C-13

---
+++ 0x1C-binary_trees/13-binary_tree_nodes.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - return number of nodes with one child in a binary tree
+ *
+ * @tree: tree to get size of
+ *
+ * Return: size of tree, 0 if NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	char onechild = 0;
+	if (tree == NULL)
+		return (0);
+	if (tree->left != NULL || tree->right != NULL)
+		onechild = 1;
+	return (binary_tree_nodes(tree->left) +
+		binary_tree_nodes(tree->right) + onechild);
+}
