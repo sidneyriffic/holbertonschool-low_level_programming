6517d736574e01e4f3527b6e67a4dd708b9bd7b9
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-02 13:04:05 -0700

    Betty 0x1C-0

--- 0x1C-binary_trees/0-binary_tree_node.c
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
-/*
+/**
  * binary_tree_node - add a node to a tree given a parent node and value
  *
  * @parent: parent node
@@ -14,10 +14,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
 	if (new == NULL)
-		return NULL;
+		return (NULL);
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
-	return new;
+	return (new);
 }
