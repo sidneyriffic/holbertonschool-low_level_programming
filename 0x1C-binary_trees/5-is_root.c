72f17609fbb3230990bbc6833a2bf0af385306bc
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-02 14:00:56 -0700

    Add 0x0C-5

---
+++ 0x1C-binary_trees/5-is_root.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - return whether a node is a root or not
+ *
+ * @node: node to check
+ *
+ * Return: 1 if root, 0 otherwise
+ */
+int binary_tree_is_root(binary_tree_t *node)
+{
+	if (node != NULL && node->parent == NULL)
+		return (1);
+	return (0);
+}
