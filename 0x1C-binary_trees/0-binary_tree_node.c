268cf6b429c3dbb4c72235f1e5796ae8f035108b
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-02 12:57:11 -0700

    Add first files for 0x1C-0 to verify proper base setup

---
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/*
+ * binary_tree_node - add a node to a tree given a parent node and value
+ *
+ * @parent: parent node
+ * @value: value of new node
+ *
+ * Return: address of new node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL)
+		return NULL;
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	return new;
+}
---
+++ 0x1C-binary_trees/README.md
@@ -0,0 +1,1 @@
+#create and manipulate a binary tree
