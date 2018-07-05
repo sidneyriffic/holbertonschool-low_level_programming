b5ea7bc58bab74dc12cab2e565920de5c7a09c39
Sidney Burkett <sidneyriffic@gmail.com>
2018-07-05 04:16:19 -0700

    Add 0x1C-16

---
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -0,0 +1,53 @@
+#include "binary_trees.h"
+
+/**
+ * get_nodes_perfect - get height of tree while checking for fullness
+ *
+ * @tree: tree to check
+ * @nodes: count of nodes
+ *
+ * Return: whether tree is balanced
+ */
+int get_height_perfect(binary_tree_t *tree)
+{
+	size_t height_left, height_right;
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if ((tree->left == NULL) != (tree->right == NULL))
+		return (0);
+	height_left = get_height_perfect(tree->left);
+	if (height_left == 0)
+		return (0);
+	height_right = get_height_perfect(tree->right);
+	if (height_right == 0)
+		return (0);
+	return (height_left + height_right + 1);
+}
+
+
+/**
+ * binary_tree_is_perfect - return whether a binary tree is perfect
+ *
+ * @tree: tree to check for perfection
+ *
+ * Return: 1 for perfect, 0 for not
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t height_left, height_right;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if ((tree->left == NULL) != (tree->right == NULL))
+		return (0);
+	height_left = get_height_perfect(tree->left);
+	if (height_left == 0)
+		return (0);
+	height_right = get_height_perfect(tree->right);
+	if (height_right != height_left)
+		return (0);
+	return (1);
+}
